ea5a5b18f1531b7578ddb77ee3b73cf30c882778
Vineet <naikvin@gmail.com>
2014-02-05 21:45:36 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+with open('./README.rst') as f:
+    long_desc = f.read()
+
+
+setup(
+    name='pipdeptree',
+    version='0.1',
+    author='Vineet Naik',
+    author_email='naikvin@gmail.com',
+    url='https://github.com/naiquevin/pipdeptree',
+    license='MIT License',
+    description='Command line utility to show dependency tree of packages',
+    long_description=long_desc,
+    install_requires=["pip >= 1.4.1"],
+    py_modules=['pipdeptree'],
+    entry_points={
+        'console_scripts': [
+            'pipdeptree = pipdeptree:main'
+        ]
+    }
+)
