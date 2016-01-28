275335df3e556b09e3495d81080906d91e197348
Adam Chainz <adam@adamj.eu>
2016-01-28 17:06:16 +0000

    Add Python 3.5 support

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ]
 )
