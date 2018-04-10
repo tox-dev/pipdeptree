1a25ae1ade820a12a17057a357c48a3f251e081a
Patrick Wang <mail6543210@yahoo.com.tw>
2018-04-10 22:04:53 +0800

    Implement Package.__lt__ to make it comparable

--- pipdeptree.py
+++ pipdeptree.py
@@ -119,6 +119,9 @@ class Package(object):
     def __repr__(self):
         return '<{0}("{1}")>'.format(self.__class__.__name__, self.key)
 
+    def __lt__(self, rhs):
+        return self.key < rhs.key
+
 
 class DistPackage(Package):
     """Wrapper class for pkg_resources.Distribution instances
