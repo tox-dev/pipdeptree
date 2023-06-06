c010e73a966f0536831479ef23ff0300c563cdfc
Kemal Zebari <60799661+kemzeb@users.noreply.github.com>
2023-06-05 18:53:32 -0700

    Use unicode characters when rendering text (#239)

--- tests/test_pipdeptree.py
+++ tests/test_pipdeptree.py
@@ -224,11 +224,138 @@ def test_req_package_as_dict():
 # Tests for render_text
 
 
+class MockStdout:
+    """
+    A wrapper to stdout that mocks the `encoding` attribute (to have `render_text()` render with unicode/non-unicode)
+    and `write()` (so that `print()` calls can write to stdout).
+    """
+
+    def __init__(self, encoding):
+        self.stdout = sys.stdout
+        self.encoding = encoding
+
+    def encoding(self):
+        return self.encoding
+
+    def write(self, text):
+        self.stdout.write(text)
+
+
 @pytest.mark.parametrize(
-    ("list_all", "reverse", "expected_output"),
+    ("list_all", "reverse", "unicode", "expected_output"),
     [
         (
             True,
+            False,
+            True,
+            [
+                "a==3.4.0",
+                "├── b [required: >=2.0.0, installed: 2.3.1]",
+                "│   └── d [required: >=2.30,<2.42, installed: 2.35]",
+                "│       └── e [required: >=0.9.0, installed: 0.12.1]",
+                "└── c [required: >=5.7.1, installed: 5.10.0]",
+                "    ├── d [required: >=2.30, installed: 2.35]",
+                "    │   └── e [required: >=0.9.0, installed: 0.12.1]",
+                "    └── e [required: >=0.12.1, installed: 0.12.1]",
+                "b==2.3.1",
+                "└── d [required: >=2.30,<2.42, installed: 2.35]",
+                "    └── e [required: >=0.9.0, installed: 0.12.1]",
+                "c==5.10.0",
+                "├── d [required: >=2.30, installed: 2.35]",
+                "│   └── e [required: >=0.9.0, installed: 0.12.1]",
+                "└── e [required: >=0.12.1, installed: 0.12.1]",
+                "d==2.35",
+                "└── e [required: >=0.9.0, installed: 0.12.1]",
+                "e==0.12.1",
+                "f==3.1",
+                "└── b [required: >=2.1.0, installed: 2.3.1]",
+                "    └── d [required: >=2.30,<2.42, installed: 2.35]",
+                "        └── e [required: >=0.9.0, installed: 0.12.1]",
+                "g==6.8.3rc1",
+                "├── e [required: >=0.9.0, installed: 0.12.1]",
+                "└── f [required: >=3.0.0, installed: 3.1]",
+                "    └── b [required: >=2.1.0, installed: 2.3.1]",
+                "        └── d [required: >=2.30,<2.42, installed: 2.35]",
+                "            └── e [required: >=0.9.0, installed: 0.12.1]",
+            ],
+        ),
+        (
+            True,
+            True,
+            True,
+            [
+                "a==3.4.0",
+                "b==2.3.1",
+                "├── a==3.4.0 [requires: b>=2.0.0]",
+                "└── f==3.1 [requires: b>=2.1.0]",
+                "    └── g==6.8.3rc1 [requires: f>=3.0.0]",
+                "c==5.10.0",
+                "└── a==3.4.0 [requires: c>=5.7.1]",
+                "d==2.35",
+                "├── b==2.3.1 [requires: d>=2.30,<2.42]",
+                "│   ├── a==3.4.0 [requires: b>=2.0.0]",
+                "│   └── f==3.1 [requires: b>=2.1.0]",
+                "│       └── g==6.8.3rc1 [requires: f>=3.0.0]",
+                "└── c==5.10.0 [requires: d>=2.30]",
+                "    └── a==3.4.0 [requires: c>=5.7.1]",
+                "e==0.12.1",
+                "├── c==5.10.0 [requires: e>=0.12.1]",
+                "│   └── a==3.4.0 [requires: c>=5.7.1]",
+                "├── d==2.35 [requires: e>=0.9.0]",
+                "│   ├── b==2.3.1 [requires: d>=2.30,<2.42]",
+                "│   │   ├── a==3.4.0 [requires: b>=2.0.0]",
+                "│   │   └── f==3.1 [requires: b>=2.1.0]",
+                "│   │       └── g==6.8.3rc1 [requires: f>=3.0.0]",
+                "│   └── c==5.10.0 [requires: d>=2.30]",
+                "│       └── a==3.4.0 [requires: c>=5.7.1]",
+                "└── g==6.8.3rc1 [requires: e>=0.9.0]",
+                "f==3.1",
+                "└── g==6.8.3rc1 [requires: f>=3.0.0]",
+                "g==6.8.3rc1",
+            ],
+        ),
+        (
+            False,
+            False,
+            True,
+            [
+                "a==3.4.0",
+                "├── b [required: >=2.0.0, installed: 2.3.1]",
+                "│   └── d [required: >=2.30,<2.42, installed: 2.35]",
+                "│       └── e [required: >=0.9.0, installed: 0.12.1]",
+                "└── c [required: >=5.7.1, installed: 5.10.0]",
+                "    ├── d [required: >=2.30, installed: 2.35]",
+                "    │   └── e [required: >=0.9.0, installed: 0.12.1]",
+                "    └── e [required: >=0.12.1, installed: 0.12.1]",
+                "g==6.8.3rc1",
+                "├── e [required: >=0.9.0, installed: 0.12.1]",
+                "└── f [required: >=3.0.0, installed: 3.1]",
+                "    └── b [required: >=2.1.0, installed: 2.3.1]",
+                "        └── d [required: >=2.30,<2.42, installed: 2.35]",
+                "            └── e [required: >=0.9.0, installed: 0.12.1]",
+            ],
+        ),
+        (
+            False,
+            True,
+            True,
+            [
+                "e==0.12.1",
+                "├── c==5.10.0 [requires: e>=0.12.1]",
+                "│   └── a==3.4.0 [requires: c>=5.7.1]",
+                "├── d==2.35 [requires: e>=0.9.0]",
+                "│   ├── b==2.3.1 [requires: d>=2.30,<2.42]",
+                "│   │   ├── a==3.4.0 [requires: b>=2.0.0]",
+                "│   │   └── f==3.1 [requires: b>=2.1.0]",
+                "│   │       └── g==6.8.3rc1 [requires: f>=3.0.0]",
+                "│   └── c==5.10.0 [requires: d>=2.30]",
+                "│       └── a==3.4.0 [requires: c>=5.7.1]",
+                "└── g==6.8.3rc1 [requires: e>=0.9.0]",
+            ],
+        ),
+        (
+            True,
+            False,
             False,
             [
                 "a==3.4.0",
@@ -264,6 +391,7 @@ def test_req_package_as_dict():
         (
             True,
             True,
+            False,
             [
                 "a==3.4.0",
                 "b==2.3.1",
@@ -298,6 +426,7 @@ def test_req_package_as_dict():
         (
             False,
             False,
+            False,
             [
                 "a==3.4.0",
                 "  - b [required: >=2.0.0, installed: 2.3.1]",
@@ -318,6 +447,7 @@ def test_req_package_as_dict():
         (
             False,
             True,
+            False,
             [
                 "e==0.12.1",
                 "  - c==5.10.0 [requires: e>=0.12.1]",
@@ -334,11 +464,13 @@ def test_req_package_as_dict():
         ),
     ],
 )
-def test_render_text(capsys, list_all, reverse, expected_output):
+def test_render_text(capsys, list_all, reverse, unicode, expected_output):
     tree = t.reverse() if reverse else t
-    p.render_text(tree, list_all=list_all, frozen=False)
-    captured = capsys.readouterr()
-    assert "\n".join(expected_output).strip() == captured.out.strip()
+    encoding = "utf-8" if unicode else "ascii"
+    with mock.patch("sys.stdout", MockStdout(encoding)):
+        p.render_text(tree, list_all=list_all, frozen=False)
+        captured = capsys.readouterr()
+        assert "\n".join(expected_output).strip() == captured.out.strip()
 
 
 # Tests for graph outputs
