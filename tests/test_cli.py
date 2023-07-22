616618fd7d35a417ff052de69472f2dc6e523986
Kemal Zebari <60799661+kemzeb@users.noreply.github.com>
2023-07-22 09:51:53 -0700

    Have --packages and --all work together (#273)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Bernát Gábor <bgabor8@bloomberg.net>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import pytest
 
-from pipdeptree._cli import build_parser
+from pipdeptree._cli import build_parser, get_options
 
 
 def test_parser_default() -> None:
@@ -75,3 +75,25 @@ def test_parser_depth(should_be_error: bool, depth_arg: list[str], expected_valu
     else:
         args = parser.parse_args(depth_arg)
         assert args.depth == expected_value
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        pytest.param(["--exclude", "py", "--all"], id="exclude-all"),
+        pytest.param(["-e", "py", "--packages", "py2"], id="exclude-packages"),
+        pytest.param(["-e", "py", "-p", "py2", "-a"], id="exclude-packages-all"),
+    ],
+)
+def test_parser_get_options_exclude_combine_not_supported(args: list[str], capsys: pytest.CaptureFixture[str]) -> None:
+    with pytest.raises(SystemExit, match="2"):
+        get_options(args)
+
+    out, err = capsys.readouterr()
+    assert not out
+    assert "cannot use --exclude with --packages or --all" in err
+
+
+def test_parser_get_options_exclude_only() -> None:
+    parsed_args = get_options(["--exclude", "py"])
+    assert parsed_args.exclude == "py"
