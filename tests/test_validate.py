0d3718e1e7e3640bc685bab1b8131b5ee35b4edf
Kemal Zebari <kemalzebra@gmail.com>
2023-07-29 17:57:37 -0700

    Detect dependency cycles beyond a depth = 2

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -16,35 +16,67 @@ if TYPE_CHECKING:
 @pytest.mark.parametrize(
     ("mpkgs", "expected_keys", "expected_output"),
     [
-        (
+        pytest.param(
             {
                 ("a", "1.0.1"): [("b", [(">=", "2.0.0")])],
                 ("b", "2.3.0"): [("a", [(">=", "1.0.1")])],
                 ("c", "4.5.0"): [("d", [("==", "2.0")])],
                 ("d", "2.0"): [],
             },
-            [("a", "b", "a"), ("b", "a", "b")],
+            [["a", "b", "a"], ["b", "a", "b"]],
             ["Warning!! Cyclic dependencies found:", "* b => a => b", "* a => b => a"],
+            id="depth-of-2",
         ),
-        (  # if a dependency isn't installed, cannot verify cycles
+        pytest.param(
+            {
+                ("a", "1.0.1"): [("b", [(">=", "2.0.0")]), ("e", [("==", "2.0")])],
+                ("b", "2.3.0"): [("c", [(">=", "4.5.0")])],
+                ("c", "4.5.0"): [("d", [("==", "1.0.1")])],
+                ("d", "1.0.0"): [("a", [("==", "1.0.1")]), ("e", [("==", "2.0")])],
+                ("e", "2.0"): [],
+            },
+            [
+                ["b", "c", "d", "a", "b"],
+                ["c", "d", "a", "b", "c"],
+                ["d", "a", "b", "c", "d"],
+                ["a", "b", "c", "d", "a"],
+            ],
+            [
+                "Warning!! Cyclic dependencies found:",
+                "* b => c => d => a => b",
+                "* c => d => a => b => c",
+                "* d => a => b => c => d",
+                "* a => b => c => d => a",
+            ],
+            id="depth-greater-than-2",
+        ),
+        pytest.param(
+            {("a", "1.0.1"): [("b", [(">=", "2.0.0")])], ("b", "2.0.0"): []},
+            [],
+            [],
+            id="no-cycle",
+        ),
+        pytest.param(
             {
                 ("a", "1.0.1"): [("b", [(">=", "2.0.0")])],
             },
             [],
-            [],  # no output expected
+            [],
+            id="dependency-not-installed",
         ),
+        pytest.param({("a", "1.0.1"): []}, [], [], id="no-dependencies"),
     ],
 )
 def test_cyclic_deps(
     capsys: pytest.CaptureFixture[str],
     mpkgs: dict[tuple[str, str], list[tuple[str, list[tuple[str, str]]]]],
-    expected_keys: list[tuple[str, ...]],
+    expected_keys: list[list[str]],
     expected_output: list[str],
     mock_pkgs: Callable[[MockGraph], Iterator[Mock]],
 ) -> None:
     tree = PackageDAG.from_pkgs(list(mock_pkgs(mpkgs)))
     result = cyclic_deps(tree)
-    result_keys = [(a.key, b.key, c.key) for (a, b, c) in result]
+    result_keys = [[dep.key for dep in deps] for deps in result]
     assert sorted(expected_keys) == sorted(result_keys)
     render_cycles_text(result)
     captured = capsys.readouterr()
