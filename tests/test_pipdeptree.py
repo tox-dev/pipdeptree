5ce21b457a661791e3faf71153610181591c16dc
Stanislav Filin <stasfilin@hotmail.com>
2024-06-15 09:18:59 +0200

    Move test to test_pipdeptree.py Start to use ScriptRunner to run --python=auto
    Signed-off-by: Stanislav Filin <stasfilin@hotmail.com>

--- tests/test_pipdeptree.py
+++ tests/test_pipdeptree.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
 import sys
-from subprocess import check_call  # noqa: S404
+from subprocess import CompletedProcess, check_call  # noqa: S404
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from pathlib import Path
+
     from pytest_console_scripts import ScriptRunner
+    from pytest_mock import MockFixture
 
 
 def test_main() -> None:
@@ -15,3 +18,23 @@ def test_main() -> None:
 def test_console(script_runner: ScriptRunner) -> None:
     result = script_runner.run(["pipdeptree", "--help"])
     assert result.success
+
+
+def test_main_log_resolved(tmp_path: Path, mocker: MockFixture, script_runner: ScriptRunner) -> None:
+    valid_sys_path = str([str(tmp_path)])
+
+    mocker.patch("pipdeptree._detect_env.detect_active_interpreter", return_value=str(tmp_path))
+    mocker.patch("pipdeptree._detect_env.os.environ", {"VIRTUAL_ENV": str(tmp_path)})
+    mocker.patch("pipdeptree._detect_env.Path.exists", return_value=True)
+
+    mock_subprocess_run = mocker.patch("subprocess.run")
+    mock_subprocess_run.return_value = CompletedProcess(
+        args=["python", "-c", "import sys; print(sys.path)"],
+        returncode=0,
+        stdout=valid_sys_path,
+        stderr="",
+    )
+
+    result = script_runner.run(["pipdeptree", "--python", "auto"])
+    assert result.stderr
+    assert result.stderr.startswith(f"(resolved python: {tmp_path!s}")
