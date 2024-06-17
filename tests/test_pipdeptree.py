c2245d60509f03de10c01ff9f9b3e4068cadb3ff
Stanislav Filin <stasfilin@hotmail.com>
2024-06-17 21:44:58 +0200

    Print resolved interpreter when using env detection (#379)
    Signed-off-by: Stanislav Filin <stasfilin@hotmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Kemal Zebari <60799661+kemzeb@users.noreply.github.com>

--- tests/test_pipdeptree.py
+++ tests/test_pipdeptree.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 
 import sys
-from subprocess import check_call  # noqa: S404
+from subprocess import CompletedProcess, check_call  # noqa: S404
 from typing import TYPE_CHECKING
 
+from pipdeptree.__main__ import main
+
 if TYPE_CHECKING:
+    from pathlib import Path
+
+    import pytest
     from pytest_console_scripts import ScriptRunner
+    from pytest_mock import MockFixture
 
 
 def test_main() -> None:
@@ -15,3 +21,21 @@ def test_main() -> None:
 def test_console(script_runner: ScriptRunner) -> None:
     result = script_runner.run(["pipdeptree", "--help"])
     assert result.success
+
+
+def test_main_log_resolved(tmp_path: Path, mocker: MockFixture, capsys: pytest.CaptureFixture[str]) -> None:
+    mocker.patch("sys.argv", ["", "--python", "auto"])
+    mocker.patch("pipdeptree.__main__.detect_active_interpreter", return_value=str(tmp_path))
+    mock_subprocess_run = mocker.patch("subprocess.run")
+    valid_sys_path = str([str(tmp_path)])
+    mock_subprocess_run.return_value = CompletedProcess(
+        args=["python", "-c", "import sys; print(sys.path)"],
+        returncode=0,
+        stdout=valid_sys_path,
+        stderr="",
+    )
+
+    main()
+
+    captured = capsys.readouterr()
+    assert captured.err.startswith(f"(resolved python: {tmp_path!s}")
