f1d559ffc0c438719731c9aaba2e2c448c9720f5
Kemal Zebari <kemalzebra@gmail.com>
2025-11-09 20:07:30 +0000

    Update to text output check in __main__

--- src/pipdeptree/__main__.py
+++ src/pipdeptree/__main__.py
@@ -76,9 +76,9 @@ def main(args: Sequence[str] | None = None) -> int | None:
 
 
 def _is_text_output(options: Options) -> bool:
-    if any([options.json, options.json_tree, options.graphviz_format]):
+    if any([options.json, options.json_tree, options.graphviz_format, options.mermaid]):
         return False
-    return options.output_format not in {"json", "json-tree"} and not options.output_format.startswith("graphviz-")
+    return options.output_format in {"freeze", "text"}
 
 
 def _determine_return_code(warning_printer: WarningPrinter) -> int:
